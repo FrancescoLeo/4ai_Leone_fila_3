edc17e06943b4979f366da46734ca4fd3fe8cdf0
4a <4a@pianodue16>
2023-01-21 11:39:06 +0100

    Versione base

---
+++ 4ai_Leone_fila_3/src/NomiCognomi.java
@@ -0,0 +1,17 @@
+
+/**
+ * 
+ * Scrivete una syso con i vostro nomi e cognomi
+ * 
+ * @author Leone
+ *
+ */
+
+public class NomiCognomi {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
